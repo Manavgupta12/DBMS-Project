ee0399bd12ae1df32912eef9dd802886c1a4aa8c
Manav Gupta <manavanant703@gmail.com>
2025-04-06 02:05:48 +0530

    Delete DCL.sql

--- DCL.sql
+++
@@ -1,7 +0,0 @@
--- ✅ Data Control Language: Granting and revoking privileges
-
--- Grant permissions to a user
-GRANT SELECT, INSERT ON Booking TO 'airline_user'@'localhost';
-
--- Revoke permission
-REVOKE INSERT ON Booking FROM 'airline_user'@'localhost';
